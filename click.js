cfbe71408cab090e15e6b57f51e5d797ad64c966
M Joshua Ryan <luser.droog@gmail.com>
2023-09-07 10:45:26 -0500

    Update click.js
    respond to attribute changes after states are interpreted

--- click.js
+++ click.js
@@ -1,7 +1,14 @@
 const clickBoxTemplate = document.createElement( "template" );
 clickBoxTemplate.innerHTML = `
   <style>
+    :host {
+      --bg-color: var(--table-bg, white);
+      --fg-color: var(--text, black);
+    }
     input{
+      color: var(--fg-color);
+      border-color: var(--fg-color);
+      background-color: var(--bg-color);
       text-align: center;
       cursor: default;
     }
@@ -18,21 +25,12 @@ class ClickBox extends HTMLElement {
     super();
     this.attachShadow( {mode:"open"} );
     this.shadowRoot.appendChild( clickBoxTemplate.content.cloneNode(true) );
-    if(  this.hasAttribute( "cycle" )  ){
-      this._states =
-	( this.hasAttribute("states") ?
-	    this.getAttribute("states") : "empty true false" ).split( " " );
-      console.log( this._states );
-      this._stateNumber = 0;
-      this._state = this._states[ this._stateNumber ];
-      let box = this.shadowRoot.querySelector("#box");
-      let size = this._states.
-        map(x=>this.hasAttribute(x)?this.getAttribute(x):x).
-        map(x=>x.length).
-        reduce( max );
-      console.log( size );
-      box.setAttribute( "size", size );
-    }
+
+  }
+  setValue( val ){
+    this._state = val;
+    this._stateNumber = this._states.findIndex( x => x == val );
+    this.draw();
   }
   increment() {
     this._stateNumber = (this._stateNumber + 1) % this._states.length;
@@ -40,15 +38,13 @@ class ClickBox extends HTMLElement {
   }
   draw() {
     let box = this.shadowRoot.querySelector("#box");
-    console.log( box );
+    //console.log( box );
     this.value = this._state;
-    console.log( this.value );
+    //console.log( this.value );
+    this.label =
+      this.hasAttribute( this.value )  ? this.getAttribute( this.value )  : this.value;
     box.toggleAttribute( "readonly" );
-    if(  this.hasAttribute( this.value )  ){
-      box.value = this.getAttribute( this.value );
-    } else {
-      box.value = this.value;
-    }
+      box.value = this.label;
     box.toggleAttribute( "readonly" );
     box.blur();
   }
@@ -60,9 +56,36 @@ class ClickBox extends HTMLElement {
   }
   connectedCallback() {
     this.shadowRoot.querySelector("#box").addEventListener( "click", (e)=>this.click(e) );
+
+    if(  this.hasAttribute( "cycle" )  ){
+      this._states =
+	( this.hasAttribute("states") ?
+	    this.getAttribute("states") : "empty true false" ).split( " " );
+      //console.log( this._states );
+      this._stateNumber = 0;
+      this._state = this._states[ this._stateNumber ];
+      let box = this.shadowRoot.querySelector("#box");
+      let size = this._states.
+        map(x=>this.hasAttribute(x)?this.getAttribute(x):x).
+        map(x=>x.length).
+        reduce( max );
+      //console.log( size );
+      box.setAttribute( "size", size );
+      if(  this.hasAttribute( "value" )  ){
+        this.setValue( this.getAttribute( "value" ) );
+      }
+    }
   }
   disconnectedCallback() {
     this.shadowRoot.querySelector("#box").removeEventListener();
+  }
+  static get observedAttributes() {
+    return ["value"];
+  }
+  attributeChangedCallback( name, oldValue, newValue ){
+    if(  name == "value"  ){
+      if(  this.states  ) this.setValue( newValue );
+    }
   }
 }
 
